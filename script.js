886d32023d585c7443e8c2313cbfd42627963e6f
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-25 13:11:40 +0100

    began to did a home work

---
+++ script.js
@@ -0,0 +1,27 @@
+[
+    {
+        title: "Michael",
+        description:
+            "Michael works as a programmer at Google. He has been working there for 7 years. Likes gingerbread.",
+        url:
+            "https://www.comnews.ru/sites/default/files2019/styles/large/public/articles/2019-10/%D0%A0%D1%83%D0%B4%D0%BE%D0%B2_%D0%BA%D0%B2.jpg?itok=Wu2t8z1r",
+    },
+    {
+        title: "Howard",
+        description:
+            "Howard works as a programmer at Google. He has been working there for 5 years. Likes to play Starcraft",
+        url: "https://bugaga.ru/uploads/posts/2019-10/1569918345_lica-1.jpg",
+    },
+    {
+        title: "Emma",
+        description:
+            "Emma works as a programmer at Amazoon. He has been working there for 2 years. likes to go shopping",
+        url: "https://intrigue.dating/wp-content/cache/thumb/ee/0091f0d00529dee_350x0.jpg",
+    },
+    {
+        title: "Diana",
+        description:
+            "Diana works as a programmer at Yandex. He has been working there for 9 years. likes to make cakes",
+        url: "https://zachiska.com/wp-content/uploads/2019/12/73414958_2429105324085170_2500640154078220638_n.jpg",
+    },
+]
