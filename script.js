686801343786988225eb90b65358d2dffb1c72a9
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-28 14:03:45 +0100

    tried to do home work

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-[
-    {
-        title: "Michael",
-        description:
-            "Michael works as a programmer at Google. He has been working there for 7 years. Likes gingerbread.",
-        url:
-            "https://www.comnews.ru/sites/default/files2019/styles/large/public/articles/2019-10/%D0%A0%D1%83%D0%B4%D0%BE%D0%B2_%D0%BA%D0%B2.jpg?itok=Wu2t8z1r",
-    },
-    {
-        title: "Howard",
-        description:
-            "Howard works as a programmer at Google. He has been working there for 5 years. Likes to play Starcraft",
-        url: "https://bugaga.ru/uploads/posts/2019-10/1569918345_lica-1.jpg",
-    },
-    {
-        title: "Emma",
-        description:
-            "Emma works as a programmer at Amazoon. He has been working there for 2 years. likes to go shopping",
-        url: "https://intrigue.dating/wp-content/cache/thumb/ee/0091f0d00529dee_350x0.jpg",
-    },
-    {
-        title: "Diana",
-        description:
-            "Diana works as a programmer at Yandex. He has been working there for 9 years. likes to make cakes",
-        url: "https://zachiska.com/wp-content/uploads/2019/12/73414958_2429105324085170_2500640154078220638_n.jpg",
-    },
-]
+let row = document.querySelector(".row");
+let col3 = document.querySelector(".col-3");
+row.append(col3);
+let card = document.querySelector("#card-1");
+col3.append(card);
+card.style.width = "300px";
+card.style.backgroundColor = "white";
+let cardImg = document.querySelector("#card-img");
+let cardImg2 = document.createElement("img");
+cardImg2.src = "michael.png";
+cardImg2.style.width = "300px";
+cardImg2.style.height = "300px";
+cardImg.append(cardImg2);
+card.append(cardImg);
+let cardContent = document.querySelector("#card-content");
+let h2 = document.createElement("h2");
+h2.textContent = "Michael";
+cardContent.prepend(h2);
+h2.style.textAlign = "center";
+card.append(cardContent);
+let p = document.createElement("p");
+p.textContent =
+  "Michael works as a programmer at Google. He has been working there for 7 years. Likes gingerbread.";
+p.style.padding = "0px 10px 10px 10px";
+cardContent.append(p);
